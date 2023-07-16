11ae42c0f8869ce0f34ea2281a9bd4a2b725530e
杨琼璞 <jeffygisgreat@gmail.com>
2023-07-16 21:02:43 +0800

    :coffin: remove haeder help part

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -3,7 +3,6 @@ import {observer, inject} from "mobx-react";
 import classnames from "classnames";
 
 import File from "../component/MenuLeft/File";
-import Help from "../component/MenuLeft/Help";
 import Tutorial from "../component/MenuLeft/Tutorial";
 import Pattern from "../component/MenuLeft/Pattern";
 import Function from "../component/MenuLeft/Function";
@@ -39,7 +38,6 @@ class Navbar extends Component {
           <Theme token={token} />
           <CodeTheme />
           <Setting />
-          <Help />
           <Tutorial />
         </div>
         <div className="nice-right-nav" />
