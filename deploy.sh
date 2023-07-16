6729b699675ed785794cffc405e8d2aa6d99cb6d
杨琼璞 <jeffygisgreat@gmail.com>
2023-07-16 20:51:11 +0800

    :rocket: increase deploy script

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+yarn build
+
+# 进入生成的文件夹
+cd ./build
+
+# 如果是发布到自定义域名
+echo 'wechat.jeffjade.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy update 🎉'
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:nicejade/markdown-nice.git master:gh-pages
+cd -
