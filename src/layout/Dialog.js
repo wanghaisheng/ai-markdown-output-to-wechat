b315524ed378854a4dd344a33aa475c6ec41defe
杨琼璞 <jeffygisgreat@gmail.com>
2023-07-17 23:55:46 +0800

    :coffin: remove about Dialog

--- src/layout/Dialog.js
+++ src/layout/Dialog.js
@@ -1,7 +1,6 @@
 import React, {Component} from "react";
 import ImageDialog from "../component/Dialog/ImageDialog";
 import LinkDialog from "../component/Dialog/LinkDialog";
-import AboutDialog from "../component/Dialog/AboutDialog";
 import FormDialog from "../component/Dialog/FormDialog";
 import HistoryDialog from "../component/Dialog/HistoryDialog";
 import SitDownDialog from "../component/Dialog/SitDownDialog";
@@ -13,7 +12,6 @@ class Dialog extends Component {
       <div>
         <ImageDialog />
         <LinkDialog />
-        <AboutDialog />
         <FormDialog />
         <HistoryDialog />
         <SitDownDialog />
